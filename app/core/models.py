659472ea25fa2f23aed1ab0b4a78501d1ba10265
Boldizsar <boldizsar.kormendy@gmail.com>
2022-06-12 17:04:10 +0200

    Fixed a bunch of test errors, except ONE FUUUUUUUUUUUU

--- app/core/models.py
+++ app/core/models.py
@@ -42,15 +42,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
 class Recipe(models.Model):
-    """This is a recipe object"""
-    user=models.ForeignKey(
+    """Recipe object."""
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    description=models.TextField(blank=True)
-    time_minutes=models.IntegerField(default=0)
-    price=models.DecimalField(max_digits=5, decimal_places=2)
-    title=models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.title
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,12 +8,25 @@ from core.models import Recipe
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    """View for managing recipe API """
-    serializer_class=serializers.RecipeSerializer
-    queryset=Recipe.objects.all()
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[IsAuthenticated]
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
 
     def get_queryset(self):
         """Retrive recipes for authenticated user"""
-        return self.queryset.filter(username=self.request.user).order_by('-id')
+        return self.queryset.filter(user=self.request.user).order_by('-id').distinct()
+
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def preform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
